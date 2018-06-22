524b5b9248977b669abe7ede9a5f8c1d9f86faf5
Administrator <Administrator@KSWi7>
2018-06-22 15:19:23 +0900

    페이지 업데이트

--- 01DataTansfer/src/ServerListFrame.java
+++ 01DataTansfer/src/ServerListFrame.java
@@ -1,11 +1,75 @@
-import javax.swing.JFrame;
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.List;
 
-public class ServerListFrame extends JFrame
+import javax.swing.Box;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JTextField;
+
+public class ServerListFrame
 {
-    ServerListFrame()
+    private JFrame frame;
+    private JTextField textField;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args)
     {
-        setSize(500, 500);
-        setVisible(true);   // 화면에 프레임 창을 표시
-        System.out.println("ServerListFrame 실행됨");
+        EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    ServerFrame window = new ServerFrame();
+                    window.frame.setVisible(true);
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public ServerListFrame()
+    {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize()
+    {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 999, 715);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+        
+        JLabel label = new JLabel("현재위치");
+        label.setBounds(33, 25, 57, 15);
+        frame.getContentPane().add(label);
+        
+        textField = new JTextField();
+        textField.setBounds(102, 22, 496, 21);
+        frame.getContentPane().add(textField);
+        textField.setColumns(10);
+        
+        List list = new List();
+        list.setBounds(31, 59, 567, 392);
+        frame.getContentPane().add(list);
+        
+        Component horizontalStrut = Box.createHorizontalStrut(20);
+        horizontalStrut.setBounds(33, 461, 744, 134);
+        frame.getContentPane().add(horizontalStrut);
+        
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
     }
 }
